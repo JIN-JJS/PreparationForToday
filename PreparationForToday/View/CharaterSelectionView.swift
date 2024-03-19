5fe7748d9165b4ff7f4ade28370e281c4a214e08
JIN-JJS <joonsu2005@naver.com>
2024-03-19 16:15:06 +0900

    [Add] CharaterSelectionView

---
+++ PreparationForToday/View/CharaterSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  CharaterSelectionView.swift
+//  PreparationForToday
+//
+//  Created by 전준수 on 3/19/24.
+//
+
+import SwiftUI
+
+struct CharaterSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CharaterSelectionView()
+}
