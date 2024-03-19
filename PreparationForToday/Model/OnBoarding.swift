315417f5ffed0b4f596145f370ab88485cf775c9
JIN-JJS <joonsu2005@naver.com>
2024-03-19 13:19:31 +0900

    [Feat] Onboarding Model

--- PreparationForToday/Model/OnBoarding.swift
+++
@@ -1,8 +0,0 @@
-//
-//  OnBoarding.swift
-//  PreparationForToday
-//
-//  Created by 전준수 on 3/19/24.
-//
-
-import Foundation
---
+++ PreparationForToday/Model/Onboarding.swift
@@ -0,0 +1,15 @@
+//
+//  OnBoarding.swift
+//  PreparationForToday
+//
+//  Created by 전준수 on 3/19/24.
+//
+
+import Foundation
+
+struct Onboarding: Identifiable, Hashable {
+    var id: Int
+    var systemName: String = ""
+    var title: String = ""
+    var content: String = ""
+}
