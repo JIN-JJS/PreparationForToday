2636e0f0c4078b355fce762a699530c16bea7768
JIN-JJS <joonsu2005@naver.com>
2024-03-19 17:27:26 +0900

    [Add] AcceptLocationServicesView

---
+++ PreparationForToday/View/AcceptLocationServicesView.swift
@@ -0,0 +1,18 @@
+//
+//  AcceptLocationServicesView.swift
+//  PreparationForToday
+//
+//  Created by 전준수 on 3/19/24.
+//
+
+import SwiftUI
+
+struct AcceptLocationServicesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AcceptLocationServicesView()
+}
