e6ddd08a3f5f29cfced3648530cdd86df00d843c
JIN-JJS <joonsu2005@naver.com>
2024-03-19 17:19:22 +0900

    [Chore] CharaterSelectionView
    Select Button chore

--- PreparationForToday/View/CharaterSelectionView.swift
+++ PreparationForToday/View/CharaterSelectionView.swift
@@ -15,82 +15,98 @@ struct CharaterSelectionView: View {
     
     var body: some View {
         
+        
         VStack {
             
-            Text("Gender")
-                .multilineTextAlignment(.center)
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            
-            if genderNumber == 0 {
-                Image(systemName: "mustache.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(.black)
-                    .frame(width:200,height: 200)
-                
-            } else if genderNumber == 1 {
-                Image(systemName: "mouth.fill")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(.black)
-                    .frame(width:200,height: 200)
-                
-            } else if genderNumber == 2 {
-                VStack {}
-                .frame(width:200,height: 200)
-                
-            }
-            
-            
-            HStack {
-                Spacer()
-                
-                Button {
-                    genderNumber = 0
-                } label: {
-                    Text("Male")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.gray)
-                }
-                
-                Spacer()
-                
-                Button {
-                    genderNumber = 1
-                } label: {
-                    Text("Female")
-                        .font(.title2)
-                        .fontWeight(.bold)
-                        .foregroundColor(.gray)
-                }
-                
-                Spacer()
-            }
-            .frame(width: 330)
-            .padding()
-            
-            Button{
-                if genderNumber == 0 {
-                    characterSelectionViewModel.selectMale()
-                } else if genderNumber == 1 {
-                    characterSelectionViewModel.selectFemale()
-                } else if genderNumber == 2 {
-                    
-                }
-            } label: {
-                Text("선택하기")
+            VStack {
+                Text("Gender")
+                    .multilineTextAlignment(.center)
+                    .font(.largeTitle)
                     .fontWeight(.bold)
-                    .padding(.horizontal)
-                    .padding(.vertical, 6)
-                    .frame(maxWidth: 330)
             }
-            .buttonStyle(.borderedProminent)
-            .padding(.bottom,24)
+            .frame(width: 350, height: 40)
+            .padding(.vertical,48)
+            
+            Spacer()
+            
+            VStack {
+                
+                HStack {
+                    if genderNumber == 0 {
+                        Image(systemName: "mustache.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.brown)
+                            .frame(width:200,height: 200)
+                    } else if genderNumber == 1 {
+                        Image(systemName: "mouth.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.red)
+                            .frame(width:200,height: 200)
+                    } else if genderNumber == 2 {
+                        VStack {}
+                            .frame(width:200,height: 200)
+                    }
+                }
+                
+                Spacer()
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    Button {
+                        genderNumber = 0
+                    } label: {
+                        Text("Male")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        genderNumber = 1
+                    } label: {
+                        Text("Female")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                            .foregroundColor(.gray)
+                    }
+                    
+                    Spacer()
+                }
+                .frame(width: 350)
+                .padding()
+                
+                Spacer()
+                
+                HStack {
+                    if genderNumber == 0 || genderNumber == 1 {
+                        Button{
+                            if genderNumber == 0 {
+                                characterSelectionViewModel.selectMale()
+                            } else if genderNumber == 1 {
+                                characterSelectionViewModel.selectFemale()
+                            }
+                        } label: {
+                            Text("선택하기")
+                                .fontWeight(.bold)
+                                .padding(.horizontal)
+                                .padding(.vertical, 6)
+                                .frame(maxWidth: 330)
+                        }
+                        .buttonStyle(.borderedProminent)
+                    }
+                }
+                .frame(width: 350, height: 70)
+                
+            }
+            .frame(width: 350, height: 600)
         }
-      
-        
+        .padding()
     }
 }
 
--- PreparationForToday/View/OnboardingView.swift
+++ PreparationForToday/View/OnboardingView.swift
@@ -13,55 +13,59 @@ struct OnboardingView: View {
     
     var body: some View {
         VStack{
-                   VStack{
-                       Text("외출 준비를 해볼까요?")
-                           .multilineTextAlignment(.center)
-                           .font(.largeTitle)
-                           .fontWeight(.bold)
-                   }
-                   .padding(.vertical,48)
-                   
-                   VStack(spacing:30){
-                       
-                       ForEach(onboardingViewModel.OnboardingModelList){ index in
-                           HStack{
-                               Image(systemName: "\(index.systemName)")
-                                   .resizable()
-                                   .scaledToFit()
-                                   .foregroundColor(.accentColor)
-                                   .frame(width:40,height: 40)
-                                   .padding(8)
-                               
-                               VStack{
-                                   Text("\(index.title)")
-                                       .font(.headline)
-                                       .fontWeight(.bold)
-                                       .frame(maxWidth: .infinity,alignment: .leading)
-                                       .padding(.bottom,0.25)
-                                   Text("\(index.content)")
-                                       .font(.subheadline)
-                                       .foregroundColor(.gray)
-                                       .lineLimit(3)
-                                       .frame(maxWidth: .infinity,alignment: .leading)
-                               }
-                           }.padding(.horizontal, 16)
-                       }
-                       Spacer()
-                       
-                       
-                       Button{
-                           isFirstLaunch = false
-                       } label: {
-                           Text("시작하기")
-                               .fontWeight(.bold)
-                               .padding(.horizontal)
-                               .padding(.vertical, 6)
-                               .frame(maxWidth: 330)
-                       }
-                       .buttonStyle(.borderedProminent)
-                       .padding(.bottom,24)
-                   }
-               }.padding()
+            VStack{
+                Text("외출 준비를 해볼까요?")
+                    .multilineTextAlignment(.center)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+            }
+            .frame(width: 350, height: 40)
+            .padding(.vertical,48)
+            
+            VStack(spacing:30){
+                
+                ForEach(onboardingViewModel.OnboardingModelList){ index in
+                    HStack{
+                        Image(systemName: "\(index.systemName)")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.accentColor)
+                            .frame(width:40,height: 40)
+                            .padding(8)
+                        
+                        VStack{
+                            Text("\(index.title)")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .frame(maxWidth: .infinity,alignment: .leading)
+                                .padding(.bottom,0.25)
+                            Text("\(index.content)")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                                .lineLimit(3)
+                                .frame(maxWidth: .infinity,alignment: .leading)
+                        }
+                    }.padding(.horizontal, 16)
+                }
+                Spacer()
+                
+                HStack {
+                    Button{
+                        isFirstLaunch = false
+                    } label: {
+                        Text("시작하기")
+                            .fontWeight(.bold)
+                            .padding(.horizontal)
+                            .padding(.vertical, 6)
+                            .frame(maxWidth: 330)
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
+                .frame(width: 350, height: 70)
+                
+            }
+            .frame(width: 350, height: 600)
+        }.padding()
     }
 }
 
