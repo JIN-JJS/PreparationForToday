4ad9802dbd1abd2a04096a4e4b3de9d9a293b7b6
JIN-JJS <joonsu2005@naver.com>
2024-03-22 22:46:47 +0900

    [Add] SettingServicesView

---
+++ PreparationForToday/View/SettingServicesView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingServicesView.swift
+//  PreparationForToday
+//
+//  Created by 전준수 on 3/22/24.
+//
+
+import SwiftUI
+
+struct SettingServicesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingServicesView()
+}
