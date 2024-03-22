3ef2b127e3254ba68d122c3b3c705e0fd7d99507
JIN-JJS <joonsu2005@naver.com>
2024-03-22 22:45:31 +0900

    [Add] WeatherServicesView

---
+++ PreparationForToday/View/WeatherServicesView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherServicesView.swift
+//  PreparationForToday
+//
+//  Created by 전준수 on 3/22/24.
+//
+
+import SwiftUI
+
+struct WeatherServicesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherServicesView()
+}
