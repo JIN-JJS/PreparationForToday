0cbbfa504f79ddc607fe231354e6cea894404f68
JIN-JJS <joonsu2005@naver.com>
2024-03-22 22:27:55 +0900

    [Fix] ContentView branch processing bug

--- PreparationForToday/View/ContentView.swift
+++ PreparationForToday/View/ContentView.swift
@@ -20,25 +20,22 @@ struct ContentView: View {
             // MARK: 2.성별 선택뷰
             // 데이터에 성별 값이 none으로 되어 있는 경우 성별 선택뷰로 넘어감
             CharaterSelectionView()
-            
                 .fullScreenCover(isPresented: $isFirstLaunch) {
                     // MARK: 1.온보딩뷰
                     // 첫 실행 시, 온보딩뷰로 넘어감
                     OnboardingView(isFirstLaunch: $isFirstLaunch)
                 }
         } else {
-            if locationViewModel.userLocation == nil {
-                
-                if locationViewModel.authorisationStatus != .denied {
-                    // MARK: 3. 위치 서비스뷰
-                    // 위치 서비스 공유를 아직 묻지 않았을 경우 넘어감
-                    AcceptLocationServicesView()
-                } else {
-                    Text("deniedView")
-                }
-              
-                
-            } else if locationViewModel.userLocation != nil {
+            if locationViewModel.authorisationStatus == .notDetermined || locationViewModel.authorisationStatus == .restricted {
+                // MARK: 3-1. 위치 서비스뷰
+                // 위치 서비스 권한 요청 뷰로 이동
+                AcceptLocationServicesView()
+            } else if locationViewModel.authorisationStatus == .denied {
+                // MARK: 3-2. 위치 서비스 거부뷰
+                // 위치 서비스 거부로 인한 수동 요청뷰 로 이동
+                DeniedLocationServicesView()
+            } else if locationViewModel.authorisationStatus == .authorizedAlways || locationViewModel.authorisationStatus == .authorizedWhenInUse {
+                // MARK: 4. 메인뷰
                 VStack {
                     Label(weatherKitViewModel.temp, systemImage: weatherKitViewModel.symbol)
                 }
@@ -46,9 +43,7 @@ struct ContentView: View {
                     weatherKitViewModel.getWeather(latitude: locationViewModel.latitude, longitude: locationViewModel.longitude)
                 }
             }
-           
         }
-
     }
 }
 
--- PreparationForToday/View/DeniedLocationServicesView.swift
+++ PreparationForToday/View/DeniedLocationServicesView.swift
@@ -9,7 +9,89 @@ import SwiftUI
 
 struct DeniedLocationServicesView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            
+            VStack {
+                Text("위치 서비스 설정")
+                    .multilineTextAlignment(.center)
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+            }
+            .frame(width: 350, height: 40)
+            .padding(.vertical,48)
+            
+            
+            VStack {
+                Text("위치 서비스 사용 권한을 수동으로 허용해주세요.")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                Spacer()
+                
+                Text("1. 설정 > 개인정보 보호 및 보안 > 위치 서비스로 이동")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                Spacer()
+                
+                Text("2. 위치 서비스가 켜져 있는지 확인합니다.")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                Spacer()
+                
+                Text("3. 아래로 스크롤하여 앱을 찾습니다.")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                Spacer()
+                
+                Text("4. 앱을 탭하고 다음과 같은 옵션을 선택합니다.")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                Spacer()
+                
+                Text("- 안함: 위치 서비스 정보에 대한 접근을 차단합니다.")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                Spacer()
+                
+                Text("- 다음번에 묻기 또는 내가 공유할 때: '앱을 사용하는 동안 허용', '한 번 허용' 또는 '허용 안함'을 선택할 수 있습니다.")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                
+                Spacer()
+                
+                Text("- 앱을 사용하는 동안: 앱 또는 앱 기능 중 하나가 화면에 표시될 때만 위치 서비스 접근을 허용합니다.")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .lineLimit(3)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+            }
+            .frame(width: 350, height: 600)
+        }
+        .padding()
     }
 }
 
