4047788d657726a109079ca128e6f3b541d6433f
JIN-JJS <joonsu2005@naver.com>
2024-03-22 21:30:01 +0900

    [Add] Add DeniedLocationServicesView

---
+++ PreparationForToday/View/DeniedLocationServicesView.swift
@@ -0,0 +1,18 @@
+//
+//  DeniedLocationServicesView.swift
+//  PreparationForToday
+//
+//  Created by 전준수 on 3/22/24.
+//
+
+import SwiftUI
+
+struct DeniedLocationServicesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeniedLocationServicesView()
+}
